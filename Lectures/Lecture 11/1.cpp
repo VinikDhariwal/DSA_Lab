26e38504341b00696dc22866ded00dcbe2686cbc
VinikDhariwal <vinikdhariwal04@gmail.com>
2026-02-02 15:47:25 +0530

    Lecture 11

---
+++ Lectures/Lecture 11/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=7845543;
+    long b =987654;
+    long long c=123456789012345;
+    string s="Hello, World!";
+    double d=3.14159;
+    float f=2.71828;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << s << endl;
+    cout << d * 8.749 << endl;
+    cout << f * 5.1122 << endl;
+    return 0;
+}
