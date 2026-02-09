6c2dc5107bb6cfb89f7136629d6fa21221e03b6f
VinikDhariwal <vinikdhariwal04@gmail.com>
2026-02-09 23:23:04 +0530

    Patterns

---
+++ Patterns/4.cpp
@@ -0,0 +1,16 @@
+//right angle triangle from left for numbers
+#include <iostream>
+using namespace std;
+void print(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    print(n);
+}
