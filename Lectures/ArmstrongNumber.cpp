3e89b65c6e6c38e55560e347664a90063e180d8e
VinikDhariwal <vinikdhariwal04@gmail.com>
2026-02-11 16:22:02 +0530

    Lecture 20

---
+++ Lectures/ArmstrongNumber.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int og=n;
+    int temp=n;
+    int sum=0;
+    int l=0;
+    while(temp!=0){
+        temp=temp/10;
+        l++;
+    }
+    while(n!=0){
+        int k=n%10;
+        sum=sum+pow(k,l);
+        n=n/10;
+    }
+    if(og==sum){
+        cout<<"Armstrong Number";
+    }else{
+        cout<<"Not Armstrong Number";
+    }
+    return 0;
+}
